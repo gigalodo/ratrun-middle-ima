82f8a53e06f899292f0a6288df6c139afbfbdaa1
Your Name <you@example.com>
2023-05-23 21:08:17 +0900

    feat: ヘッダの作成

---
+++ components/Organisms/Header/AppHeader.vue
@@ -0,0 +1,47 @@
+<!-- 
+<template>
+ <Header>
+<p>Headerのコンポーネントです。！；；</p>
+ </Header>
+</template>
+-->
+
+<template>
+  <header>
+    <nav>
+      <li v-for="(link, key) in linkList" :key="key">
+        <nuxt-link :to="link.to">{{ link.text }}</nuxt-link>
+      </li>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  setup() {
+    const linkList = reactive([
+      { text: "ホーム", to: "#" },
+      { text: "経営理念", to: "#" },
+      { text: "事業理念", to: "#" },
+      { text: "事業目的", to: "#" },
+      { text: "問い合わせ", to: "#" },
+    ]);
+    return { linkList };
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 20px;
+}
+li {
+  list-style: none;
+}
+a {
+  text-decoration: none;
+}
+</style>
+
+
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,20 @@
 <template>
  <div>
-   <p>ヘッダー</p>
+   <AppHeader />
    <NuxtPage />
-   <p>フッター</p>
+   <p>フッター！</p>
  </div>
 </template>
 
 
 <script>
+import{AppHeader}from"../components/Organisms/Header";
+
 export default {
+  components:{AppHeader}
 };
 </script>
+
 
 <style lang='scss' scoped>
 </style>
